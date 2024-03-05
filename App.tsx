e7b10ba792f45d1b939264676664fbed351b888d
casagrande7 <joaofootz10@gmail.com>
2024-03-05 15:15:43 -0300

    Atualizado

--- App.tsx
+++ App.tsx
@@ -1,21 +1,75 @@
 import React, { useState, useEffect } from "react";
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={require('./src/assets/imagens/crono.png')} />
+      <Text style={styles.timer}>00:00:00</Text>
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Iniciar</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+
+      </View>
+      <View style={styles.areaTempo}>
+        <Text style={styles.textoCorrida}>Último Tempo: 00:00:00
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- container: {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2F4F4F'
+  },
+  areaTempo: {
+    marginTop: 40
 
- },
- areaTempo: {
+  },
+  textoCorrida: {
+    fontSize: 20,
+    color: '#FFF',
+    fontStyle: 'italic'
 
- },
+  },
+  timer: {
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF'
+
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    width: 17,
+    borderRadius: 20
+
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+   color: '#2F4F4F'
+
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40
+    
+  },
 });
 
 export default App;
